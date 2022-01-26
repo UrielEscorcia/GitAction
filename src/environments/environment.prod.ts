1d5b465f46adf47670f0ed5b766443c68c420dc3
Uriel Escorcia <uris_ec@hotmail.com>
2022-01-26 16:11:06 -0600

    fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'actions-73e6b',
+    appId: '1:167749162914:web:8bfd455d23e6041fc384fb',
+    storageBucket: 'actions-73e6b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCm0dMzkwc1jbY_xLuZNqbFuE0vr1uelic',
+    authDomain: 'actions-73e6b.firebaseapp.com',
+    messagingSenderId: '167749162914',
+  },
   production: true
 };
