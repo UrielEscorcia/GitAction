fef800797559712d3bfa8eca45e186e603a7faf2
Uriel Escorcia <uris_ec@hotmail.com>
2022-02-06 02:07:22 -0600

    prettier conf

--- src/app/models/UIRxForms.ts
+++ src/app/models/UIRxForms.ts
@@ -1,29 +1,28 @@
- import {
-     AppFormGroup,
+import {
+    AppFormGroup,
     IAbstractControl,
     IFormGroup,
     RxFormArray,
-} from '@rxweb/reactive-form-validators';
+} from '@rxweb/reactive-form-validators'
 
 export interface IModelForm<T> extends AppFormGroup<T> {
     controls: {
-        [key in keyof T]: IAbstractControl | IFormGroup<T[key]> | RxFormArray;
-    };
+        [key in keyof T]: IAbstractControl | IFormGroup<T[key]> | RxFormArray
+    }
 }
 
-
-export function isInvalid(control?: IAbstractControl ) {
-    return control?.invalid && (control?.dirty || control?.touched);
+export function isInvalid(control?: IAbstractControl) {
+    return control?.invalid && (control?.dirty || control?.touched)
 }
 
 export function showErrorMessage(control?: IAbstractControl) {
-    return (control?.touched || control?.dirty) && control?.errors;
+    return (control?.touched || control?.dirty) && control?.errors
 }
 
 export function getErrorMessage(control?: IAbstractControl) {
     for (const err in control?.errors) {
         if (control?.errors.hasOwnProperty(err)) {
-            return control.errors[err].message;
+            return control.errors[err].message
         }
     }
     return ''
